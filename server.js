bfe2aef6926a6bab7e799bf165124138dac2c804
awang617 <wanga18@mail.wlu.edu>
2019-03-04 14:28:19 -0800

    set up server with app use and listen

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-// const
+const
+    express = require('express')
+    
+const app = express()
+
+app.use(bodyParser.urllencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(express.static('public'))
+
+app.listen(3001, () => console.log('Listening on port 3001 :)'))
