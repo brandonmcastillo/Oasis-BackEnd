8e0d8b02f3fe68d5596f11aaedaa3378df118e5c
awang617 <wanga18@mail.wlu.edu>
2019-03-04 14:28:54 -0800

    started set up for user route

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,36 @@
-// comment
+const
+    express = require('express')
+    router = express.Router(),
+    jwt = require('jsonwebtoken'),
+    // controllers = require('../controllers')
+
+router.post('/signup', controllers.user.signup);
+
+router.post('/login', controllers.user.login)
+
+router.use((req, res, next) => {
+    console.log('activated')
+    const bearerHeader = req.headers['authorization'];
+    console.log('triggered token check', bearerHeader)
+  
+    if(typeof bearerHeader !== 'undefined'){
+      const bearer = bearerHeader.split(' ');
+      const bearerToken = bearer[1];
+      req.token = bearerToken;
+      let verified = jwt.verify(req.token, '');
+      console.log('here is the verified', verified)
+      req.userId = verified._id //set user id for routes to use
+      next();
+    } 
+    else {
+      res.sendStatus(403);
+    }
+})
+
+router.get('/', controllers.user.show)
+
+router.put('/user/:id', (req,res) => {
+    return res.json
+})
+
+module.exports = router
