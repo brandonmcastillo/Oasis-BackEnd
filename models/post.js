3e85a3f9843b4dd5d38d6deb3f3a22afbec1720c
Michael_Claus <mclausaudio@gmail.com>
2019-03-05 12:34:55 -0800

    fixed some typos for seeding purposes

--- models/post.js
+++ models/post.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const PostSchema = mongoose.Schema({
-    title: String,
-    content: String,
-    userId: {
-        type: mongoose.Schema.type.ObjectId,
-        ref: 'User'
-    },
-    cityId: {
-        type: mongoose.Schema.type.ObjectId,
-        ref: 'City'
-    }
-})
+  title: String,
+  content: String,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  cityId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "City"
+  }
+});
 
-module.exports = mongoose.model('Post', PostSchema)
+module.exports = mongoose.model("Post", PostSchema);
