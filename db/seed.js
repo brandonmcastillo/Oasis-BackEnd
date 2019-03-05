d6965e781bcd732215ad7b352c8362a8af86a381
awang617 <wanga18@mail.wlu.edu>
2019-03-05 09:17:30 -0800

    started adding seed data

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,22 @@
 //comment
+
+const city_list = [
+    {
+        name: 'San Francisco',
+        image: '',
+    },
+    {
+        name: 'London',
+        image: '',
+    },
+    {
+        name: 'New York',
+        image: '',
+    }
+]
+
+const user_list = [
+
+]
+
+const post_list = []
