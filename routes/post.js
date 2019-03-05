3e172390da356809f784f3b126b0720e8188a255
awang617 <wanga18@mail.wlu.edu>
2019-03-04 17:15:24 -0800

    defining post routes

--- routes/post.js
+++ routes/post.js
@@ -9,7 +9,7 @@ router.get('/:id', controllers.post.get_post)
 
 router.put('/:id', controllers.post.update)
 
-router.post('/:id', controllers.post.show)
+router.post('/', controllers.post.show)
 
 router.delete('/:id', controllers.post.remove)
 
