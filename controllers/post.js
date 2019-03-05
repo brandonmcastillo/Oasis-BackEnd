dca9fc348e4f8b53771c2e31eaee47f6028d3e23
awang617 <wanga18@mail.wlu.edu>
2019-03-04 17:13:38 -0800

    moved functions to user controller

--- controllers/post.js
+++ controllers/post.js
@@ -2,19 +2,41 @@ const db = require('../models')
 
 module.exports = {
     index: (req, res) => {
-        db.Post.find()
+        db.Post.find( (err, foundPosts) => {
+            if (err) {console.log(err)}
+            res.json(foundPosts)
+        })
     },
     get_post: (req, res) => {
-
+        let postId = req.params.id
+        db.Post.findOne( {_id: postId}, (err, foundPost) => {
+            if (err) {console.log(err)}
+            res.json(foundPost)
+        })
     },
     update: (req, res) => {
-
+        let postId = req.params.id
+        db.Post.findOneAndUpdate({_id: postId}, req.body, {new: true}, (err, updatedPost) => {
+            if (err) {console.log(err)}
+            res.json(updatedPost)
+        })
     },
     show: (req, res) => {
-
+        let newPost = new db.Post({
+            title: req.body.title,
+            content: req.body.content
+        })
+        newPost.save( (err, savedPost) => {
+            if (err) {console.log(err)}
+            res.json(savedPost)
+        })
     },
     remove: (req, res) => {
-
+        let postId = req.params.id
+        db.Post.findOneAndDelete({_id: postId}, (err, deletedPost) => {
+            if (err) {console.log(err)}
+            res.json(deletedPost)
+        })
     },
 
 }
