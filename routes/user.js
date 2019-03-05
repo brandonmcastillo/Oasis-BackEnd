d16cf4e9ca7fd1a1922119280ea1beee1a7f2390
awang617 <wanga18@mail.wlu.edu>
2019-03-04 17:17:17 -0800

    added some post routes to user

--- routes/user.js
+++ routes/user.js
@@ -29,8 +29,23 @@ router.use((req, res, next) => {
 
 router.get('/', controllers.user.show)
 
-router.get('/:id', controllers.post.index)
-router.get('/:id')
+// router.get('/:id', controllers.post.index)
+
+
+// Get all the posts made by one user
+router.get('/:id/post', controller.post.index)
+
+// Get one post by id made by one user
+router.get('/:id', controllers.post.get_post)
+
+// Update a post made by a user
+router.put('/:userId/post/:id', controllers.post.update)
+
+// Create a new post
+// router.post('/', controllers.post.create)
+
+// Delete a post
+router.delete('/:userId/post/:id', controllers.post.remove)
 
 
 module.exports = router
