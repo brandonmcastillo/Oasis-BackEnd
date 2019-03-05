687372f0f2f606e6aa7601ecdba83f16a6660f44
Amberly Wang <46389060+awang617@users.noreply.github.com>
2019-03-05 15:34:41 -0800

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/api", (req, res) => {
     message: "Wayfarer API",
     documentationURL:
       "https://github.com/brandonmcastillo/Wayfarer-BackEnd/blob/master/README.md",
-    baseURL: "", //heroku URL goes here
+    baseURL: "https://radiant-tor-11139.herokuapp.com/",
     apiEndpoint: {
       method: "GET",
       path: "/api",
