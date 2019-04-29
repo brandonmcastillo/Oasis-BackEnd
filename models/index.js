25070ebe5bf2f165c38b4bd4d40b7300ff1e6e2d
Brandon Castillo <brandonmaerina@gmail.com>
2019-04-29 15:34:16 -0700

    heroku config

--- models/index.js
+++ models/index.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/Oasis-BackEnd",
+//   {
+//     useNewUrlParser: true
+//   }
+// );
+
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Oasis-BackEnd",
+  process.env.MONGODB_URI || "mongodb://heroku_50knqwb7:m2fhtkfqob2hrvh6j87bcndlf4@ds115971.mlab.com:15971/heroku_50knqwb7",
   {
     useNewUrlParser: true
   }
 );
+
 
 module.exports = {
   City: require("./City"),
